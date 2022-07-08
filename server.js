9c45b51494eaa62a86007211adf99eb34bb01f37
negar miralaie <miralaienegar@gmail.com>
2022-07-08 17:20:16 +0430

    fixed a bug in naming

--- server.js
+++ server.js
@@ -2,17 +2,19 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 const redis = require("redis");
-const utils = require("utils");
+const util = require("util");
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
 const redisUrl = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisUrl);
-client.set = utils.promisify(client.set);
+client.set = util.promisify(client.set);
 
 app.post("/", async (req, res) => {
    const { name, number } = req.body;
+
+   //We are able to use async/await here bc we added promisify
    const response = await client.SET(name, number);
    res.json(response);
 });
