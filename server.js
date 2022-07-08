11cb3eea5458154893cb5b8a79c95960a23a1f3b
negar miralaie <miralaienegar@gmail.com>
2022-07-08 17:08:58 +0430

    added redis

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 require('dotenv').config();
-
-
+const redis = require("redis");
 const PORT = process.env.PORT || 5000;
+
+const redisUrl = "redis://127.0.0.1:6379";
+const client = redis.createClient(redisUrl);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
