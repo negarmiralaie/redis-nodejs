bda8391ff3cefc325ccd57d624a3d1256d73265b
negar miralaie <miralaienegar@gmail.com>
2022-07-08 17:16:46 +0430

    using promisify

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 const redis = require("redis");
+const utils = require("utils");
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
 const redisUrl = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisUrl);
+client.set = utils.promisify(client.set);
 
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
    const { name, number } = req.body;
-   client.SET(name, number);
+   const response = await client.SET(name, number);
+   res.json(response);
 });
 
 app.listen(PORT, () => {
