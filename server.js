7465a11d3ac56cd74cdd19ae676168ae4f5aa49c
negar miralaie <miralaienegar@gmail.com>
2022-07-08 17:12:07 +0430

    additional express setup and a simple rout handling

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ require('dotenv').config();
 const redis = require("redis");
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
 const redisUrl = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisUrl);
+
+app.post("/", (req, res) => {
+   const { name, number } = req.body;
+   client.SET(name, number);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
